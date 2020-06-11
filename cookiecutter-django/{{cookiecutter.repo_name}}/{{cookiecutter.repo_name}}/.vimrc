5c174150b063d78927e67d62b599b7d9f9904a31
Daniele Pira <daniele.pira@otto.to.it>
2020-06-11 09:45:10 +0200

    first example of compose django cookiecutter

--- cookiecutter-django/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/.vimrc
+++
@@ -1,2 +0,0 @@
-autocmd FileType python set ft=python.django
-autocmd FileType html set ft=htmldjango
--- cookiecutter-django/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.core_name}}/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-
-# custom user model just in case we need to add functionality
-class User(AbstractUser):
-    pass
