e6b0c98c62ae07559bc5ae5a08038aeb887941e0
abidibo <abidibo@gmail.com>
2020-06-15 17:11:38 +0200

    adds start script

--- {{cookiecutter.repo_name}}/cli.py
+++ {{cookiecutter.repo_name}}/cli.py
@@ -8,14 +8,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Otto Web Application CLI')
     parser.add_argument(
-        '-r',
-        '--run',
-        dest='run',
+        '--start',
+        dest='start',
         required=False,
         action='store_true',
-        help='runserver')
+        help='starts the environment')
     parser.add_argument(
-        '-s',
+        '--runserver',
+        dest='runserver',
+        required=False,
+        action='store_true',
+        help='execs a django runserver command')
+    parser.add_argument(
         '--shell',
         dest='open_shell',
         required=False,
@@ -23,7 +27,6 @@ if __name__ == "__main__":
         help='opens a shell in the app container'
     )
     parser.add_argument(
-        '-c',
         '--createsuperuser',
         dest='createsuperuser',
         required=False,
@@ -32,9 +35,11 @@ if __name__ == "__main__":
     )
     args = vars(parser.parse_args())
 
-    if args['open_shell']:
+    if args['start']:
+        os.system("docker-compose -f docker-compose.yml up")
+    elif args['open_shell']:
         os.system("docker-compose -f docker-compose.yml exec app bash")
-    elif args['run']:
+    elif args['runserver']:
         os.system("docker-compose -f docker-compose.yml exec app -w /home/app/{{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }} bash -c \"source ../../venv/bin/activate && python manage.py runserver\"")
     elif args['createsuperuser']:
         os.system("docker-compose -f docker-compose.yml exec app -w /home/app/{{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }} bash -c \"source ../../venv/bin/activate && export DJANGO_SUPERUSER_PASSWORD=admin && python manage.py createsuperuser --no-input --username admin --email kkk@otto.to.it\"")
